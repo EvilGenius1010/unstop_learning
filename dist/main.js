87ec703f402d67c866a3973bd0df227dd2c509ce
quirkyNashedi <harshavardhan.mitblr2022@learner.manipal.edu>
2024-10-16 00:43:02 +0530

    implemented signup and login

--- dist/main.js
+++ dist/main.js
@@ -8,29 +8,71 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const prisma_1 = __importDefault(require("./prisma"));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Content-Type", "application/json");
     // res.setHeader("Access-Control-Allow-Credentials", true);
     // res.setHeader("Access-Control-Allow-Private-Network", true);
     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
     // res.setHeader("Access-Control-Max-Age", 7200);
     next();
 });
+app.use(express.json());
 app.post('/signup', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.json({
-        msg: "fuck you"
+    console.log(req.body);
+    const getinfo = yield prisma_1.default.user.findUnique({
+        where: {
+            username: req.body.username,
+            // password: req.body.password
+        }
     });
+    if (getinfo == null) {
+        const pushinfo = yield prisma_1.default.user.create({
+            data: {
+                username: req.body.username,
+                password: req.body.password
+            }
+        });
+        res.json({
+            msg: "Account created successfully."
+        });
+    }
+    else {
+        res.json({
+            msg: "Username already exists."
+        });
+    }
 }));
-app.post('login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.json({
-        msg: "nigger go to /signup first! now piss off"
+app.post('/login', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const checkcreds = yield prisma_1.default.user.findUnique({
+        where: {
+            username: req.body.username,
+            password: req.body.password
+        }
     });
+    if ((checkcreds === null || checkcreds === void 0 ? void 0 : checkcreds.username) == req.body.username && (checkcreds === null || checkcreds === void 0 ? void 0 : checkcreds.password) == req.body.password) {
+        res.json({
+            msg: "Login successful."
+        });
+    }
+    else if (checkcreds == null) {
+        res.json({
+            msg: "Incorrect username or password."
+        });
+    }
+    // res.json({
+    //   msg: checkcreds
+    // })
 }));
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 import { Request, Response } from "express";
-
-
+import prisma from "./prisma";
 
 app.use((req: Request, res: Response, next: any) => {
   res.setHeader(
@@ -18,6 +17,7 @@ app.use((req: Request, res: Response, next: any) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
+  res.setHeader("Content-Type", "application/json")
   // res.setHeader("Access-Control-Allow-Credentials", true);
   // res.setHeader("Access-Control-Allow-Private-Network", true);
   //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
@@ -26,16 +26,60 @@ app.use((req: Request, res: Response, next: any) => {
   next();
 });
 
-app.post('/signup', async (req: any, res: any) => {
-  res.json({
-    msg: "fuck you"
+app.use(express.json())
+
+
+app.post('/signup', async (req: Request, res: Response) => {
+  console.log(req.body)
+  const getinfo = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+      // password: req.body.password
+    }
   })
+
+  if (getinfo == null) {
+
+    const pushinfo = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: req.body.password
+      }
+    })
+    res.json({
+      msg: "Account created successfully."
+    })
+  }
+  else {
+    res.json({
+      msg: "Username already exists."
+    })
+  }
+
 })
 
-app.post('login', async (req: any, res: any) => {
-  res.json({
-    msg: "nigger go to /signup first! now piss off"
+app.post('/login', async (req: any, res: any) => {
+
+  const checkcreds = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+      password: req.body.password
+    }
   })
+
+  if (checkcreds?.username == req.body.username && checkcreds?.password == req.body.password) {
+    res.json({
+      msg: "Login successful."
+    })
+  } else if (checkcreds == null) {
+    res.json({
+      msg: "Incorrect username or password."
+    })
+  }
+
+  // res.json({
+  //   msg: checkcreds
+  // })
 })
 
 app.listen(port, () => {
