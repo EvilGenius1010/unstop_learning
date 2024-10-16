8db92f4517493466bef0a8f8aeb66b229a0d4796
quirkyNashedi <harshavardhan.mitblr2022@learner.manipal.edu>
2024-10-16 17:03:13 +0530

    changed port bindings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT
 import { Request, Response } from "express";
 import prisma from "./prisma";
 const axios = require('axios')
